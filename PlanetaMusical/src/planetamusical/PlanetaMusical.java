2b936b953da021bc5556314bdec1cc48ab92c6ed
Sandro <blosomsam@gmail.com>
2016-06-18 16:59:12 -0500

    HOla mundo enviado a desarollo

--- PlanetaMusical/src/planetamusical/PlanetaMusical.java
+++ PlanetaMusical/src/planetamusical/PlanetaMusical.java
@@ -6,7 +6,7 @@ public class PlanetaMusical {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
